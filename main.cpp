b904002bc5fb6c09966ac307a205af25146fca9f
60y0ung <60y0ung@naver.com>
2016-02-17 19:06:08 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include "Value.h"
+#include "GameMgr.h"
+
+LRESULT CALLBACK WndProc( HWND, UINT, WPARAM, LPARAM);
+
+HINSTANCE g_hInst;
+LPCTSTR lpszClass =TEXT("first");
+HWND hWnd;
+bool g_bExit;
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	CGameMgr* m_pGame = new CGameMgr();
+
+	srand((unsigned int)time(NULL));
+	
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = (WNDPROC)WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	DWORD dwTime = GetTickCount();
+	DWORD dwCount = 1000;
+
+	bool on = false;
+
+	m_pGame->Init();
+
+	while (!g_bExit)
+	{
+		if (PeekMessage(&Message, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&Message);
+			DispatchMessage(&Message);
+		}
+		else
+		{
+			if (dwTime + dwCount < GetTickCount())
+			{
+				dwTime = GetTickCount();
+				on = true;
+			}
+
+			if (m_pGame != NULL)
+			{
+				m_pGame->Move();
+				m_pGame->Draw(on);
+				on = false;
+			}
+		}
+
+	}
+
+	m_pGame->Destroy();
+	return Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	switch(iMessage)
+	{
+	case WM_DESTROY:
+		g_bExit = true;
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
