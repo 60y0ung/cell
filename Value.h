80b0c36ffd5b7eed4a9e635a30ada38b4076d4f3
60y0ung <60y0ung@naver.com>
2016-03-02 16:48:51 +0900

    Create Value.h

---
+++ Value.h
@@ -0,0 +1,17 @@
+#include <Windows.h>
+#include <time.h>
+#include <list>
+#include <math.h>
+#include <cmath>
+#include <stdio.h>
+
+
+using namespace std;
+
+extern HWND hWnd;
+
+#define WINSIZEX 800
+#define WINSIZEY 600
+
+#define PI 3.1415926535
+
