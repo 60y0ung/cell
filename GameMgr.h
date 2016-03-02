cc238d1fcd01cdb6cb7fd156de97f29839935732
60y0ung <60y0ung@naver.com>
2016-03-02 16:49:14 +0900

    Update GameMgr.h

--- GameMgr.h
+++ GameMgr.h
@@ -16,14 +16,17 @@ public :
 	void Draw( void );
 	void Destroy( void );
 
+private:
+	void RenderAngle(HDC hdc);
+
 
 protected :
 	DWORD dwTime;
 	DWORD dwCount;
+	HDC hdc, backDC;
+	HBITMAP backBit, OldBit;
 
 	list<CPray*> m_pPray;
 
 	CPlayer* m_pPlayer;
-
 };
-
