2874bf5fa0b8f7f9a2fc9a1fb86cee97180a2824
60y0ung <60y0ung@naver.com>
2016-03-02 16:51:00 +0900

    Update player.h

--- player/player.h
+++ player/player.h
@@ -12,6 +12,16 @@ public:
 	void OnFrameMove(const float c_fElaspedTime);
 	void OnFrameRender(HDC hdc);
 
+	inline void SetR(int r)
+	{
+		m_iR += r;
+	}
+
 protected :
 	int m_iSpeed;
+
+protected :
+	void Arrive(void);
+	double GetAngle(int x1, int y1, int x2, int y2);
+
 };
